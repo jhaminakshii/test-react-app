1ec9c785f7fa700121b7f30cff971b0077c54114
Minakshi <jhaminakshi333@gmail.com>
2023-09-21 20:04:18 +0545

    Using router method

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@
 import { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
-//import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
  import TxtForms from './components/TxtForms';
+ import {
+   BrowserRouter as Router,
+   Route,
+   Routes,
+ } from "react-router-dom";
 
 function App() {
   const [mode,setMode] = useState("light");
@@ -33,10 +38,17 @@ function App() {
   }
   return (
     <>
-      <Navbar title="TextEditor" aboutText="About Us" mode={mode} handleMode={handleMode} />
+    <Router>
+    <Navbar title="TextEditor" aboutText="About Us" mode={mode} handleMode={handleMode} />
       <Alert alert={alert}/>
-      <TxtForms heading="Enter the Text to Analyze Below" mode={mode} showAlert={showAlert} />
-      {/* <About/> */}
+      <Routes>
+          <Route exact path="/about" element={<About/>}/>
+            </Routes>
+            <Routes>
+          <Route exact path="/" element={ <TxtForms heading="Enter the Text to Analyze Below" mode={mode} showAlert={showAlert} />}/>
+        </Routes>
+    </Router>
+
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import PropTypes from 'prop-types'
+import { Link } from "react-router-dom";
 
 
 export default function Navbar(props) {
   return (
     <div>
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/">
           {props.title}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -24,37 +25,14 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/">
+              <Link className="nav-link" to="/">
                 Home <span className="sr-only">(current)</span>
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <Link className="nav-link" to="/about">
                 {props.aboutText}
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="/"
-                role="button"
-                data-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Services
-              </a>
-              <div className="dropdown-menu">
-                <a className="dropdown-item" href="/">
-                  Action
-                </a>
-                <a className="dropdown-item" href="/">
-                  Another action
-                </a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="/">
-                  Something else here
-                </a>
-              </div>
+              </Link>
             </li>
           </ul>
           <div className="form-check form-switch">
