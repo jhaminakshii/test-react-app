a85c2f04fed99ff242220d5cbc85fe7fab4702cd
Minakshi <jhaminakshi333@gmail.com>
2023-09-21 10:47:47 +0545

    enable dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TxtForms from './components/TxtForms';
+// import TxtForms from './components/TxtForms';
 
 function App() {
   return (
     <>
    <Navbar title="TextEditor" aboutText="About Us" />
-    <TxtForms heading = "Enter the Text to Analyze Below" />
+    {/* <TxtForms heading = "Enter the Text to Analyze Below" /> */}
+    <About/>
     </>
   );
 }
